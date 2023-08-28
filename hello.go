0a24fa112021d5fff85c8dadec5af2d1d3a85083
titi-lima <titisau@gmail.com>
2023-08-28 01:51:44 -0300

    json

--- hello.go
+++ hello.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	jsonData := map[string]string{"message": "Hello, World 👋!"}
+	jsonValue, err := json.Marshal(jsonData)
+	if err != nil {
+		panic(err)
+	}
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
+        return c.Send(jsonValue)
     })
 
     app.Listen(":3000")
